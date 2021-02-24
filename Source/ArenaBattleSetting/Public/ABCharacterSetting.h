243e69a02414173a7c94dca8baade57490cea2d3
Jiwon Kang <jwkorange@gmail.com>
2021-02-24 15:49:34 +0900

    [Add] ArenaBattleSetting Module

---
+++ Source/ArenaBattleSetting/Public/ABCharacterSetting.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ABCharacterSetting.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLESETTING_API UABCharacterSetting : public UObject
+{
+	GENERATED_BODY()
+};
